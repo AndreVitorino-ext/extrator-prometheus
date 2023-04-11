84382d59e1dfa488cf988db51dde03dfdbe2ad09
AndreVitorino-ext <andre.vitorino@viavarejo.com.br>
2023-04-11 16:40:30 -0300

    atualização extrator

---
+++ jornadas.py
@@ -0,0 +1,23 @@
+compra = [
+    {'jornada': 'Compra', 'tribo': 'vitrine', 'produto': 'carrinho', 'reqs_cb': 'carrinho-api-cb', 'reqs_ex': 'carrinho-api-ex', 'reqs_pf': 'carrinho-api-pf', 'reqs_corp': '' , 'server': 'http://prometheus-akspriv-checkout-prd.viavarejo.com.br'},
+
+    {'jornada': 'Compra', 'tribo': 'carrinho-front', 'produto': 'apollo', 'reqs_cb': 'carrinho-front-cb', 'reqs_ex': 'carrinho-front-ex', 'reqs_pf': 'carrinho-front-pf', 'reqs_corp': '' , 'server': 'http://prometheus-akspriv-checkout-prd.viavarejo.com.br'},
+
+    {'jornada': 'Compra', 'tribo': 'check', 'produto': 'checkout', 'reqs_cb': 'checkout-api-cb', 'reqs_ex': 'checkout-api-ex', 'reqs_pf': 'checkout-api-pf', 'reqs_corp': '' , 'server': 'http://prometheus-akspriv-checkout-prd.viavarejo.com.br'},
+]
+
+b2b = [
+    {'jornada': 'b2b', 'tribo': 'parcerias', 'produto': 'checkout-front', 'reqs_cb': 'checkout-front-cb', 'reqs_ex': 'checkout-front-ex', 'reqs_pf': 'checkout-front-pf', 'reqs_corp': '', 'server': 'http://prometheus-akspriv-checkout-prd.viavarejo.com.br'},
+
+    {'jornada': 'b2b', 'tribo': 'parcerias', 'produto': 'kong_proxy', 'reqs_cb': 'kong-proxy-cb-prd', 'reqs_ex': 'kong-proxy-ex-prd', 'reqs_pf': 'kong-proxy-pf-prd', 'reqs_corp': '' , 'server': 'http://prometheus-akspriv-kong-prd.viavarejo.com.br/'},
+
+    {'jornada': 'b2b', 'tribo': 'parcerias', 'produto': 'kong', 'reqs_cb': '', 'reqs_ex': '', 'reqs_pf': '', 'reqs_corp': 'kong-data-plane-proxy', 'server': 'http://prometheus-akspriv-checkout-prd.viavarejo.com.br'}
+]
+
+figital = [
+    {'jornada': 'teste', 'tribo': 'teste', 'produto': 'teste', 'reqs_cb': 'checkout-front-cb', 'reqs_ex': 'checkout-front-ex', 'reqs_pf': 'checkout-front-pf', 'reqs_corp': '' , 'server': 'http://prometheus-akspriv-checkout-prd.viavarejo.com.br'},
+
+    {'jornada': 'teste', 'tribo': 'teste', 'produto': 'teste', 'reqs_cb': '', 'reqs_ex': '', 'reqs_pf': '', 'reqs_corp': 'televendas-api-sit' , 'server': 'http://prometheus-akspriv-checkout-prd.viavarejo.com.br'},
+
+    {'jornada': 'teste', 'tribo': 'teste', 'produto': 'teste', 'reqs_cb': '', 'reqs_ex': '', 'reqs_pf': '', 'reqs_corp': 'kong-data-plane-proxy' , 'server': 'http://prometheus-akspriv-checkout-prd.viavarejo.com.br'},
+]
